71f31b194e3abc48e84f1ca8dcc352b71700a848
RotrixxLOL <ncusnir43@gmail.com>
2022-09-28 17:33:20 +0200

    add NavLink to logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,7 +28,9 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-16 object-contain" />
+        <NavLink to="/">
+          <img src={logo} alt="logo" className="w-full h-16 object-contain" />
+        </NavLink>
         <NavLinks />
       </div>
 
